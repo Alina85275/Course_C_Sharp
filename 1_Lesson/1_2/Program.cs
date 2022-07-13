2786e09076ee41e6c4ebb48043df9dbe954066c5
Alina <lixondot@mail.ru>
2022-07-13 19:54:52 +0800

    Task 1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b == a * a)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
