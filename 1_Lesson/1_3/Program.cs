ec66feb42c890d94920401c81eb433b7ff70f123
Alina <lixondot@mail.ru>
2022-07-13 20:21:03 +0800

    Tast 1_3

---
+++ 1_Lesson/1_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write a number");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("pn");
+}
+
+else if (a == 2)
+{
+  Console.WriteLine("vt");  
+}
+else if (a == 3)
+{
+  Console.WriteLine("sr");  
+}
+else if (a == 4)
+{
+  Console.WriteLine("cht");  
+}
+else if (a == 5)
+{
+  Console.WriteLine("pt");  
+}
+else if (a == 6)
+{
+  Console.WriteLine("sb");  
+}
+else if (a == 7)
+{
+  Console.WriteLine("v");  
+}
