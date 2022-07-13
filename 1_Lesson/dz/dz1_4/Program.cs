dc7ad98188193be4a483dd7ded769493e284e522
Alina <lixondot@mail.ru>
2022-07-13 22:07:48 +0800

    DZTask 1_4

---
+++ 1_Lesson/dz/dz1_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.WriteLine("Write N: ");
+string s_N = Console.ReadLine();
+
+int N = int.Parse(s_N);
+int i = 2;
+
+while (i <= N);
+{
+    Console.WriteLine(i);
+    i += 2;
+}
+
+
