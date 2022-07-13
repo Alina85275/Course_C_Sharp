c7c011958800b62fc5f984d9d7d524fb8ac35715
Alina <lixondot@mail.ru>
2022-07-13 21:34:37 +0800

    DZTask 1_2

---
+++ 1_Lesson/dz/dz1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите прграмму, уоторая принимает на вход 3 числа и выдает максимальное из этих чисел.
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+Console.WriteLine("Write b number: ");
+string s_b = Console.ReadLine();
+Console.WriteLine("Write c number: ");
+string s_c = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_c);
+if (a < b)
+{
+    a = b;
+}
+if (a < c)
+{
+    a = c;
+    }
+
+
+    Console.WriteLine(a);
+
