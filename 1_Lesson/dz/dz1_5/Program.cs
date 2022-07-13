dee3eb47b5131e06e2cc4a4aa6a62d24d94bda93
Alina <lixondot@mail.ru>
2022-07-13 22:15:01 +0800

    DZTask 1_5

---
+++ 1_Lesson/dz/dz1_5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, котрая на вход принимает на вход трехзначное число и на выходе показывает 
+// последнюю цифру этого числа.
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+
+    Console.WriteLine(a % 10);
+
+
