7fa4a14093a247e65c2b6a98282232a52097cdab
Alina <lixondot@mail.ru>
2022-07-13 21:25:21 +0800

    DZTask 1_1

---
+++ 1_Lesson/dz/dz1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает 2 числа и выдает, какое число большее, а какое меньшее.
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+Console.WriteLine("Write b number: ");
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+if (a > b)
+{
+    Console.WriteLine("a>b");
+}
+else if (a == b)
+{
+    Console.WriteLine("b=a");
+
+}
+else
+{
+    Console.WriteLine("b>a");
+}
