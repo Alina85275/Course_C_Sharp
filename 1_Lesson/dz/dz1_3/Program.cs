c98dd00f805a303518ac9c15f55039ea8ff53036
Alina <lixondot@mail.ru>
2022-07-13 21:42:17 +0800

    DZTask 1_3

---
+++ 1_Lesson/dz/dz1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, котрая на вход принимает число и выдает, является ли число четным.
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("chetnoe");
+}
+else
+{
+    Console.WriteLine("nechetnoe");;
+    }
+
