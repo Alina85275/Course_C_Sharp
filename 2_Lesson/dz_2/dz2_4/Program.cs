992f68bd133cfc3269bfabaa3ddfed0a2a61c9a7
Alina <lixondot@mail.ru>
2022-07-19 06:55:59 +0800

    DZ2_4

---
+++ 2_Lesson/dz_2/dz2_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+{ if (a == 6 || a == 7)
+ Console.WriteLine("YES");
+ else {
+   Console.WriteLine("no"); 
+ }
+
+}
