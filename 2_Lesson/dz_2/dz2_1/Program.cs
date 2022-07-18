f548a01d4f797f3e34e273160647516c02bdf14d
Alina <lixondot@mail.ru>
2022-07-19 06:22:50 +0800

    DZ2 2_1

---
+++ 2_Lesson/dz_2/dz2_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+
+    Console.WriteLine(a / 10  % 10);
