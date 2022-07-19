06445ceb52b7313f10d7ec12176eed028e12cb86
Alina <lixondot@mail.ru>
2022-07-19 22:21:52 +0800

    DZTask3_1

---
+++ 3_Lesson/DZ3/DZ3_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Kvadrat (int a)
+{
+int n = a % 10; 
+int b = a / 10000;
+int c = a / 100 % 10;
+int d = a / 1000 % 10;   
+if (a > 99999)
+{   
+Console.WriteLine("nevernoe chislo");
+
+}
+else if (n ==b || c == d)
+{
+Console.WriteLine("palindrom");
+}
+else 
+{
+ Console.WriteLine("ne palindrom");
+}
+}
+Kvadrat(33633);
