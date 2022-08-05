5371da3b52d47fd082a105c1591b0607b7997294
Alina <lixondot@mail.ru>
2022-08-06 05:39:40 +0800

    Task4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+void Dlina (int  a)
+{
+string num = a.ToString();
+Console.WriteLine(num.Length);
+}
+Dlina(478);
