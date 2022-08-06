4838214dfcbb6c8cb7c2252cef1c2cde4e9453f3
Alina <lixondot@mail.ru>
2022-08-06 18:30:08 +0800

    DZ4_2

---
+++ 4_Lesson/DZ4/DZ4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int summa (int a)
+{
+   int summ = 0;
+    while (a>0)
+{
+    
+    summ = summ + a % 10;
+    a = a/10;
+}
+return summ;
+}
+Console.WriteLine(summa(452));
