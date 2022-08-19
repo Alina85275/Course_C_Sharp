add42a4e60220bd1d277adebdffca5ab69815412
Alina <lixondot@mail.ru>
2022-08-19 20:14:01 +0800

    Task5_3

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в
+//отрезке [10,99]. 
+void print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int Nums(int[] arr)
+{
+int count = 0;
+for(int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]>= 10 && arr[i]<=99)
+    count++;
+}
+return count;
+}
+int[] arr_1 = MassNums(123, -9,10);
+print(arr_1);
+Console.WriteLine(Nums(arr_1));
