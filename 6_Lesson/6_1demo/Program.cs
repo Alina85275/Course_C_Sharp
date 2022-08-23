227a7b57b25677b3ef46a0bffe8d1082ebb347f7
Alina <lixondot@mail.ru>
2022-08-23 08:26:27 +0800

    Task6_Demo

---
+++ 6_Lesson/6_1demo/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+void print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+int[] RevvMass(int[] arr)
+{
+    int size = arr.Length -1;
+
+    for (int i = 0; i < size/2+1; i++)
+    (arr[i], arr[size - i]) = (arr[size - i], arr[i]);
+    return arr;
+}
+int[] arr_1 = MassNums(6,1,21);
+print(arr_1);
+RevvMass(arr_1);
+print(arr_1);
